948027d6cbfda9ccadfea56be5a42ec0aa7934bc
Samuel Wahinya <160012525+samuel-wahinya@users.noreply.github.com>
2025-10-26 09:41:45 +0300

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,17 @@
+-- Switch to master database
+USE master;
+
+-- Create database 'JetStar'
+CREATE DATABASE JetStar;
+
+-- Swich to JetStar
+USE JetStar;
+
+-- Create bronze schema
+CREATE SCHEMA bronze;
+
+-- Create silver schema
+CREATE SCHEMA silver;
+
+-- Create gold schema
+CREATE SCHEMA gold;
